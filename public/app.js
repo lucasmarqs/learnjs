8308697db1a34ea5754497ea48823fe8934e0691
Lucas Marques <lucas.smarques@outlook.com>
2017-04-09 23:06:01 -0300

    Have a functional navigating problems

--- public/app.js
+++ public/app.js
@@ -2,7 +2,95 @@
 
 var learnjs = {};
 
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 == 6 * __; }"
+  }
+];
+
+learnjs.applyObject = function (obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.flashElement = function (elem, content) {
+  elem.fadeOut('fast', function () {
+    elem.html(content);
+    elem.fadeIn();
+  });
+};
+
+learnjs.template = function (name) {
+  return $('.templates .' + name).clone();
+};
+
+learnjs.buildCorrectFlash = function (problemNumber) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+
+  if (problemNumber < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNumber + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+
+  return correctFlash;
+};
+
+learnjs.problemView = function (data) {
+
+  var problemNumber = parseInt(data, 10);
+  var view = learnjs.template("problem-view");
+  var problemData = learnjs.problems[problemNumber-1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer () {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick () {
+    if (checkAnswer()) {
+      learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber));
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!')
+    }
+
+    return false;
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text("Problem #" + problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber-1], view);
+
+  return view;
+};
+
+learnjs.appOnReady = function () {
+  window.onhashchange = function () {
+    learnjs.showView(window.location.hash);
+  };
+
+  learnjs.showView(window.location.hash);
+};
+
 learnjs.showView = function (hash) {
-  var problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
-}
+  var routes = {
+    '#problem': learnjs.problemView
+  };
+
+  var hashParts = hash.split('-')
+  var viewFn = routes[hashParts[0]];
+
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+};
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,8 +1,92 @@
-describe("LearnJS", function () {
-  console.log("describe a spec");
-
-  it("can show a problem view", function () {
+describe('LearnJS', function () {
+  it('can show a problem view', function () {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
+  });
+
+  it('shows the landing page view when there is no hash', function () {
+    learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+  });
+
+  it('passes the hash view parameter to the view function', function () {
+    spyOn(learnjs, 'problemView');
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
+
+  it('invokes the router when loaded', function () {
+    spyOn(learnjs, 'showView');
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
+  it('subscribes to the hash change event', function () {
+    learnjs.appOnReady();
+    spyOn(learnjs, 'showView');
+    $(window).trigger('hashchange');
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
+  describe('problem view', function () {
+    it('has a title that includes the problem number', function () {
+      var view = learnjs.problemView('1');
+      expect(view.find('.title').text()).toEqual('Problem #1');
+    });
+
+    it('invokes the applyObject function', function () {
+      spyOn(learnjs, 'applyObject');
+      learnjs.problemView('1');
+      expect(learnjs.applyObject).toHaveBeenCalled();
+    });
+  });
+
+  describe('apply object', function () {
+    it('populates element text given the object', function () {
+      var elem = learnjs.template('problem-view');
+      var obj = learnjs.problems[0];
+      learnjs.applyObject(obj, elem);
+
+      expect(elem.find('[data-name=description]').text()).toEqual(obj.description);
+      expect(elem.find('[data-name=code]').text()).toEqual(obj.code);
+    });
+  });
+
+  describe('answer section', function () {
+    var view;
+
+    beforeEach(function () {
+      view = learnjs.problemView('1');
+    });
+
+    it('can check a correct answer by hitting a button', function () {
+      spyOn(learnjs, 'buildCorrectFlash');
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(learnjs.buildCorrectFlash).toHaveBeenCalledWith(1)
+    });
+
+    it('rejects an incorrect answer', function () {
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
+    });
+  });
+
+  describe('build correct flash', function () {
+    it('creates a link to next problem', function () {
+      var view = learnjs.buildCorrectFlash(1);
+      var link = view.find('a');
+
+      expect(link.attr('href')).toEqual('#problem-2');
+    });
+
+    it('can finish the game', function () {
+      var view = learnjs.buildCorrectFlash(learnjs.problems.length);
+      var link = view.find('a');
+
+      expect(link.attr('href')).toEqual('');
+      expect(link.text()).toEqual("You're Finished!");
+    });
   });
 });
